e7706ee51392f9dacd14da7d31082d17213f0719
chandan_cloud <singhchandaninnoida@gmail.com>
2021-04-23 21:44:02 +0530

    delete node from BST

--- LogicalPrograms/src/com/unacademdy/sanketsingh/binarysearchtree/BinarySearchTree.java
+++ LogicalPrograms/src/com/unacademdy/sanketsingh/binarysearchtree/BinarySearchTree.java
@@ -43,6 +43,48 @@ public class BinarySearchTree {
 		return root;
 		}
 	
+	public static Node delete(Node root, int data) {
+		if(root == null)
+			return root;
+		
+		if(data < root.data) {
+			root.left = delete(root.left, data);
+		return root;
+		}
+		else if(data > root.data) {
+			root.right = delete(root.right, data);
+		return root;
+		}
+		else {
+			if(data == root.data) 
+			{
+			
+				if(root.left == null && root.right == null)
+					return null;
+				else if(root.left != null && root.right == null)
+					return root.left;
+				else if(root.left == null && root.right != null)
+					return root.right;
+				else {// deleting node having left and right child. Logic: search left most node of RST of root node(to be deleting node)
+					//replace root node data with RST left most node data.And then delete the left most node.
+					Node nextbig = root.right;
+					
+					while(nextbig.left != null) {
+						
+						nextbig = nextbig.left;
+					}
+						root.data = nextbig.data;
+						root.right = delete(root.right, nextbig.data);
+						return root;
+					}
+				
+				
+			}
+		}
+		
+		return root.right;
+		
+	}
 	public static void printBST(Node root) {
 		if(root == null)
 			return;
@@ -55,6 +97,9 @@ public class BinarySearchTree {
 
 		
 		Node root = buildBSTRec();
+		printBST(root);
+		delete(root, 8);
+		System.out.println("after delete");
 		printBST(root);
 		
 	}
